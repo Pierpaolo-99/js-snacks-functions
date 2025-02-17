fb7d1249420388520527d5bb9fa950d92ae830f7
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-17 15:36:43 +0100

    complete ex 4 "conta_vocali"

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function getVowels (str){
+    let arrVowels = [];
 
+    for(let i = 0; i < str.length; i++){
+        if(str[i] == 'a' || str[i] == 'i'){
+            arrVowels.push(str[i]);
+        }
+    }
+    return arrVowels
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(getVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
