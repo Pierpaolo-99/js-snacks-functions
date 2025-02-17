7c4f000892e134d3e116c5b556326e3d4cdd073d
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-17 13:57:10 +0100

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,14 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+
+const greeting = name => 'Ciao' + ' ' + name
+
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(greeting(userName));
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
