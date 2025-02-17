d9474af76ff3c82e33b9f64abe63fc8b070715de
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-17 14:44:01 +0100

    complete ex 2 "iniziali"

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,15 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-
+function getArreyInitials (array) {
+    let arrayInitials = []
+    for (var i = 0; i < array.length; i += 1) {
+        arrayInitials.push((array[i].charAt(0)));
+    }
+    return arrayInitials
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(getArreyInitials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
